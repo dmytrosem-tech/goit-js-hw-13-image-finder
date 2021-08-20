4623621aed07d7df5398ad3cb85f6d664c932c18
Dmytro Semeruk <deemoon83@gmail.com>
2021-08-20 14:43:13 +0300

    render1st

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import './sass/main.scss';
+import createMarkup from './templates/markup-tpl.hbs';
 import axios from 'axios';
 
 const refs = {
   form: document.querySelector('#form'),
   searchField: document.querySelector('#search'),
   container: document.querySelector('.container'),
+  loadMore: document.querySelector('#more'),
 };
-let page = 1;
+let currentPage = 1;
 
 const onSubmit = e => {
   e.preventDefault();
@@ -14,33 +16,20 @@ const onSubmit = e => {
   const baseApi = 'https://api.github.com';
   const myGitId = '67a962bb74ca1bbfdc52';
   const secretClient = '806def03bea1bb0c19b5389bde86a3ff499a2200';
-  fetch(
-    `${baseApi}/search/repositories?q=${submitValue}&client_id=${myGitId}&client_secret=${secretClient}&page=${page}`,
-  )
-    .then(resp => resp.json())
-    .then(data => renderCollection(data.items))
+  axios
+    .get(
+      `${baseApi}/search/repositories?q=${submitValue}&client_id=${myGitId}&client_secret=${secretClient}&page=${currentPage}`,
+    )
+    .then(result => renderCollection(result.data.items))
+    .then(() => currentPage++)
     .catch(err => console.log(err));
 };
 
-function createElementHtml({ description, owner, created_at, name, html_url, watchers_count }) {
-  const article = `<div class='repository'>
-  <h1 class='owner-title'>Owners name: ${owner.login}</h1>
-    <h2 class='rep-title'>Repository title: ${name}</h2>
-        <article class='rep__info'>
-          <img src='${owner.avatar_url}' alt='owner' class='owner__avatar'/>
-          <p class='owner__rep-create-date'>Created date: ${created_at}</p>
-          <p class='owner__watchers-count'>Watchers count: ${watchers_count}</p>
-          <a href='${html_url}' target='_blank'>Go repository</a>
-
-        </article>
-        </div>
-          
-    `;
-  refs.container.insertAdjacentHTML('beforeend', article);
-}
-
 function renderCollection(arr) {
-  arr.forEach(el => createElementHtml(el));
+  arr.forEach(el => {
+    refs.container.insertAdjacentHTML('beforeend', createMarkup(el));
+  });
 }
 
 refs.form.addEventListener('submit', onSubmit);
+refs.loadMore.addEventListener('click', onSubmit);
